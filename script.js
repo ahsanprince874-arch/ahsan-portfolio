8058039c9d023e25287ff765a32953af77f9575f
ahsanprince874-arch <ahsanprince874@gmail.com>
2026-01-05 13:53:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+
+  if (name === "" || email === "") {
+    alert("Please fill all fields");
+    return false;
+  }
+  alert("Message sent successfully!");
+  return true;
+}
